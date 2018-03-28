01fc0b0d16b353ce3f18efae66f29980906089c5
EvanXzj <2686465170@qq.com>
2018-03-28 20:37:20 +0800

    chapter 05 done

--- ch05/bitwise_complement.go
+++ ch05/bitwise_complement.go
@@ -6,4 +6,27 @@ func main() {
 	for i := 0; i <= 10; i++ {
 		fmt.Printf("the complement of %b is: %b\n", i, ^i)
 	}
+
+	for i := 0; i <= 10; i++ {
+		var v int
+		fmt.Printf("%d ", v)
+		v = 5
+	}
+
+	i := 0
+	for {
+		if i >= 3 {
+			break
+		}
+
+		fmt.Println(i)
+		i++
+	}
+
+	for i := 0; i < 7; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println(i)
+	}
 }
